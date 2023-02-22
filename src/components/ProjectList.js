9e3d3750f6b398b71f0ee6d773c7c8ac8a402889
Nicholas Brereton <nicholasrbrereton@gmail.com>
2023-02-22 13:14:21 -0500

    Lab Complete

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,12 +3,23 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+  const listProjects = projects.map((project)=> {
+    return (
+      <ProjectItem key={project.id}
+      name= {project.name}
+      about = {project.about}
+      technologies = {project.technologies} />
+    )
+  })
+  
+  
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list"> {listProjects} </div>
     </div>
-  );
+  )
 }
+
 
 export default ProjectList;
